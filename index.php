14d0ad9eae86b7862af8d00f17f9dc738e7b8761
sasadjekic <djeka78@com.com>
2021-04-22 00:27:28 +0200

    Html struktura

---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+
+        <title>Citrus commerce</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Sasa Djekic">
+        <meta name="description" content="Citrus company">
+        <meta name="keywords" content="Citrus commerce, citrus, fruit, sell, import">
+        <meta name="keywords" content="Citrus commerce, citrus, fruit, sell, import">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+              crossorigin="anonymous">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark py-3">
+            <a class="navbar-brand" href="#"><i class="far fa-lemon mr-2 d-none d-sm-inline"></i>Citrus</a>
+
+
+            <div class="collapse navbar-collapse" >
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">News</a>
+                    </li>
+                </ul>
+                <form class="form-inline ml-auto">
+
+                    <div class="input-group-append">
+                        <button class="btn btn-success" type="button">Login</button>
+                    </div>
+                </form>
+            </div>
+
+        </form>
+    </div>
+</nav>
+
+
+<div class="container">
+    <h1 class="display-1 text-center">Citrus Commerce</h1>
+    <div class="row">
+        <div class="col-xl-4 col-sm-6 mt-4">
+            <div class="card h-100">
+                <img class="card-img-top" src="images/orange.jpg" alt="Pomorandza">
+                <div class="card-body">
+                    <h5 class="card-title">Pomorandža</h5>
+                    <p class="card-text">Narandža ili pomorandža je hibrid citrusa. Nastala je
+                        ukrštanjem pomela (Citrus maxima) i mandarine (Citrus reticulata). </p>
+                    <p class="card-text"><small class="text-muted">Na lageru</small></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-sm-6 mt-4">
+            <div class="card h-100">
+                <img class="card-img-top" src="images/grape.jpg" alt="Grejpfrut">
+
+                <div class="card-body">
+                    <h5 class="card-title">Grejpfrut</h5>
+                    <p class="card-text">Grejpfrut ili grejp (lat. Citrus × paradisi) je suptropska biljka,
+                        koja je hibrid citrusa, a uzgaja se zbog ploda. Grejpfrut je zimzeleno drvo.
+                        Cvetovi su beli, petočlani, a plod je hesperidijum. Zemlja porekla ove biljke je Barbados.</p>
+                    <p class="card-text"><small class="text-muted">Na lageru</small></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-sm-6 mt-4">
+            <div class="card h-100">
+                <img class="card-img-top" src="images/lemon.jpg" alt="Limun">
+
+                <div class="card-body">
+                    <h5 class="card-title">Limun</h5>
+                    <p class="card-text">Limun (lat. Citrus x limon) je drvo iz roda Citrus (porodica rutvice (Rutaceae))
+                        i njegov plod. Rezultat je davnog ukrštanja po svoj prilici pomela i citrona, ali već
+                        vekovima uspeva kao samostalno drvo koje se razmnožava reznicama ili kalemljenjem.</p>
+                    <p class="card-text"><small class="text-muted">Za tri nedelje</small></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-sm-6 mt-4">
+            <div class="card h-100">
+                <img class="card-img-top" src="images/kumkvat.jpg" alt="Kumkvat">
+
+                <div class="card-body">
+                    <h5 class="card-title">Kumkvat</h5>
+                    <p class="card-text">Kumkvat (lat. Fortunella, Citrus japonica) je naziv za nekoliko
+                        malih voćki citrusa (rod Citrus). Plod kumkvata, hesperidijum, podseća na pomorandžu,
+                        ali je mnogo manji i tvrđi. Sličan je veličini veće maslinke.
+                    </p>
+                    <p class="card-text">
+                        <small class="text-muted">Nema trenutno na lageru</small></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-sm-6 mt-4">
+            <div class="card h-100">
+                <img class="card-img-top" src="images/pomelo.jpg" alt="Pomelo">
+
+                <div class="card-body">
+                    <h5 class="card-title">Pomelo</h5>
+                    <p class="card-text">Pomelo je neobičan član porodice južnih voćki, on je
+                        pritom posebna vrsta, ne hibrid. Neki ga nazivaju tajlandskim ili kineskim grejpfrutom.
+                        Pomelo vodi poreklo iz jugoistočne Azije.</p>
+                    <p class="card-text"><small class="text-muted">Na lageru</small></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-sm-6 mt-4">
+            <div class="card h-100">
+                <img class="card-img-top" src="images/lime.jpg" alt="Limeta">
+
+                <div class="card-body">
+                    <h5 class="card-title">Limeta</h5>
+                    <p class="card-text">Limeta, u prodavnicama često i lajm (engl. lime), naziv je za nekoliko
+                        hibrida citrusa (rod Citrus) kojima su zreli plodovi zelene boje.
+                        Plod limete, hesperidijum, je manji od ploda limuna, a podjednako je bogat vitaminom C.</p>
+                    <p class="card-text"><small class="text-muted">Na lageru sledeće nedelje</small></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-sm-6 mt-4">
+            <div class="card h-100">
+                <img class="card-img-top" src="images/tangerine.jpg" alt="Tangerine">
+
+                <div class="card-body">
+                    <h5 class="card-title">Tangerine</h5>
+                    <p class="card-text">Tangerine spada u grupu citrusnog voćanarandžaste boje
+                        koja se sastoji od hibria manarine sa nekim doprinosom pomela.
+                        Naziv je prvi put koriščćen za voće koje dolazi iz Tangera u Maroku,
+                        opisano kao sorta mandarine</p>
+                    <p class="card-text"><small class="text-muted">Na lageru</small></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-sm-6 mt-4">
+            <div class="card h-100">
+                <img class="card-img-top" src="images/grapewhite.jpg" alt="Grejp beli">
+
+                <div class="card-body">
+                    <h5 class="card-title">Grejpfrut beli</h5>
+                    <p class="card-text">Grejpfrut ili grejp (lat. Citrus × paradisi) je suptropska biljka,
+                        koja je hibrid citrusa, a uzgaja se zbog ploda. Grejpfrut je zimzeleno drvo.
+                        Cvetovi su beli, petočlani, a plod je hesperidijum. Zemlja porekla ove biljke je Barbados.</p>
+                    <p class="card-text"><small class="text-muted">Na lageru</small></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-sm-6 mt-4">
+            <div class="card h-100">
+                <img class="card-img-top" src="images/mandarine.jpg" alt="Mandarina">
+
+                <div class="card-body">
+                    <h5 class="card-title">Mandarina</h5>
+                    <p class="card-text">Mandarina (lat. Citrus reticulata) je nisko drvo iz porodice rutvice
+                        (Rutaceae), a pripada rodu citrusa. To je zimzelena biljka koja može da naraste do 3 metra.
+                        Ima šire listove od ostalih citrusa. Cvetovi beli, petočlani, a plod hesperidijum. </p>
+                    <p class="card-text"><small class="text-muted">Na lageru</small></p>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</div>
+
+<?php
+// put your code here
+?>
+</body>
+</html>
