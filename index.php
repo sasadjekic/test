94be123f739d3ecce561ae1eda50cf0ab347ef1c
sasadjekic <djeka78@com.com>
2021-04-23 00:10:43 +0200

    Komentari i PHP sa bazom proizvoda
    Sekcija za komentare
    Forma za nove komentare
    Footer
    PHP veza sa bazom i tabelom proizvoda i ispisivanje proizoda u PHP bloku

--- index.php
+++ index.php
@@ -1,9 +1,61 @@
 <!DOCTYPE html>
+<?php
 
-<html>
+class Artical {
+
+    private $id;
+    private $name;
+    private $desc;
+    private $img;
+
+    public function __get($nazivPolja) {  // nazivPolja se odnosi na dohvatanje svih vrednosti
+        return $this->$nazivPolja;      // koje imamo u klasi
+    }
+
+    public function __construct($id, $name, $desc, $img) {
+
+        $this->id = $id;
+        $this->name = $name;
+        $this->desc = $desc;
+        $this->img = $img;
+    }
+
+}
+
+function getAllArticals() { //
+    $konekcija = mysqli_connect(
+            "localhost", "root", "", "citrus");
+    $sql = "select * from articles";
+    $rezultat = mysqli_query($konekcija, $sql);  //ILI  $rezultat = $konekcija->query($sql); kao objekat
+
+    $niz = [];
+    while ($red = $rezultat->fetch_array()) {
+        $niz[] = new Artical($red['id'],
+                $red['name'],
+                $red['desc'],
+                $red['img']);
+    }
+    return $niz;
+}
+
+$rezultat = getAllArticals();
+/* var_dump($rezultat);
+
+  echo $rezultat[0]->{'id'};
+  echo $rezultat[0]->{'name'};
+  echo $rezultat[0]->{'desc'};
+  echo $rezultat[0]->{'img'};
+ */
+?>
+
+
+<html lang="sr">
     <head>
 
         <title>Citrus commerce</title>
+        <meta http-equiv="Content-Type"
+              content="text/html;
+              charset=utf-8">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="Sasa Djekic">
@@ -16,9 +68,12 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
               crossorigin="anonymous">
     </head>
+
     <body>
+
+
         <nav class="navbar navbar-expand-md navbar-dark bg-dark py-3">
-            <a class="navbar-brand" href="#"><i class="far fa-lemon mr-2 d-none d-sm-inline"></i>Citrus</a>
+            <a class="navbar-brand" href="#"><i class="far fa-lemon mr-2 d-none d-sm-inline"></i>Citrus Commerce</a>
 
 
             <div class="collapse navbar-collapse" >
@@ -28,6 +83,9 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">News</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
                     </li>
                 </ul>
                 <form class="form-inline ml-auto">
@@ -42,8 +100,44 @@
     </div>
 </nav>
 
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-5">Poštovani kupci!</h1>
+        <p class="lead">Dobrodošli na prezentaciju naše firme <b>Citrus Commerce</b> na kojoj možete saznati sve o nama i
+            proizvodima iz našeg asortimana! </p>
 
-<div class="container">
+    </div>
+</div>
+
+<div class="container mb-3">
+    <h1 class="display-1 text-center">Citrus Commerce</h1>
+    <div class="row">
+        <?php
+        $i = 0;
+        foreach ($rezultat as $el) {
+            ?>
+
+            <div class = "col-xl-4 col-sm-6 mt-4">
+                <div class = "card h-100">
+                    <img class = "card-img-top" src = "images/<?php echo $rezultat[$i]->{'img'}; ?>" alt = "Pomorandza">
+                    <div class = "card-body">
+                        <h5 class = "card-title"><?php echo $rezultat[$i]->{'name'};
+            ?></h5>
+                        <p class="card-text"><?php echo $rezultat[$i]->{'desc'}; ?> </p>
+                        <p class="card-text"><small class="text-muted">Na lageru</small></p>
+                    </div>
+                </div>
+            </div>
+            <?php
+            $i++;
+        }
+        ?>
+    </div>
+</div>
+
+
+<!--  Sekcija sa proizvodima koja nije potreban nakon sto je to prebaceno u PHP blok
+<div class="container mb-3">
     <h1 class="display-1 text-center">Citrus Commerce</h1>
     <div class="row">
         <div class="col-xl-4 col-sm-6 mt-4">
@@ -63,8 +157,7 @@
 
                 <div class="card-body">
                     <h5 class="card-title">Grejpfrut</h5>
-                    <p class="card-text">Grejpfrut ili grejp (lat. Citrus × paradisi) je suptropska biljka,
-                        koja je hibrid citrusa, a uzgaja se zbog ploda. Grejpfrut je zimzeleno drvo.
+                    <p class="card-text">Grejpfrut ili grejp (lat. Citrus × paradisi) je suptropska biljka,            koja je hibrid citrusa, a uzgaja se zbog ploda. Grejpfrut je zimzeleno drvo.
                         Cvetovi su beli, petočlani, a plod je hesperidijum. Zemlja porekla ove biljke je Barbados.</p>
                     <p class="card-text"><small class="text-muted">Na lageru</small></p>
                 </div>
@@ -72,7 +165,7 @@
         </div>
         <div class="col-xl-4 col-sm-6 mt-4">
             <div class="card h-100">
-                <img class="card-img-top" src="images/lemon.jpg" alt="Limun">
+                <img class="card-img-top" src="images/limon.jpg" alt="Limun">
 
                 <div class="card-body">
                     <h5 class="card-title">Limun</h5>
@@ -164,10 +257,61 @@
                 </div>
             </div>
         </div>
+Kraj komentara - sekcije sa Proizvodima-->
 
-
-    </div>
 </div>
+</div>
+<div class="container mb-5">
+
+    <div class="container text-left bg-primary text-white py-3 mb-3">
+        <h2 class="display-5">Drugi o nama...</h2>
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item d-flex justify-content-between">An item <span class="text-muted">2018</span></li>
+        <li class="list-group-item d-flex justify-content-between">A second item <span
+                class="text-muted">2019</span></li>
+        <li class="list-group-item d-flex justify-content-between">A third item <span class="text-muted">2019</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between">A fourth item <span
+                class="text-muted">2020</span></li>
+        <li class="list-group-item d-flex justify-content-between">And a fifth one <span
+                class="text-muted">2021</span></li>
+    </ul>
+</div>
+
+<div class="container mb-5">
+    <div class="container text-left bg-dark text-white py-3 mb-3">
+        <h2 class="display-5">Ostavite komentar</h2>
+    </div>
+    <form>
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="email" class="form-control" id="name" >
+        </div>
+        <div class="form-group">
+            <label for="mail">Email address</label>
+            <input type="email" class="form-control" id="mail" placeholder="name@example.com">
+        </div>
+
+        <div class="form-group">
+            <label for="comment">Komentar</label>
+            <textarea class="form-control" id="comment" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+
+<div class="container border pt-3 bg-secondary mb-3">
+    <footer>
+        <nav class="nav nav-pills nav-fill flex-column flex-sm-row">
+            <a class="nav-item nav-link text-white" href="#">Home</a>
+            <a class="nav-item nav-link text-white" href="#">About Us</a>
+            <a class="nav-item nav-link text-white" href="#">Contact</a>
+        </nav>
+        <p class="py-2 text-center">Copyright Citrus&copy;</p>
+    </footer>
+</div>
+
 
 <?php
 // put your code here
